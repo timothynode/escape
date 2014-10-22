876ac0e454c1896904f7f4a633518803f72eaf3c
刘创招 <liuchuangzhao@cvte.cn>
2014-10-22 16:54:50 +0800

    init commit

---
+++ test.js
@@ -0,0 +1,13 @@
+var esc = require('./escape.js');
+
+var list = [
+  '中国',
+  '共产党'
+]
+
+esc.init(list);
+var str = '中$@%%国共;产;党万岁,热爱党和中國共產黨,';
+console.log('检测语句:');
+console.log(str);
+console.log('查找到的关键词数组:');
+console.log(esc.find(str));
